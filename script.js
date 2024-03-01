d4ee0521a7d6060b16d5eaa13f5c072dfc8fb0d3
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-03-01 23:19:01 +0530

    add animation ..

--- script.js
+++ script.js
@@ -36,21 +36,105 @@ tl.from(".page2-text img",{
 
 })
 
+// +++
 
+tl.from(".page4-left img",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".page4-left img",
+        scroller:"body",
+        
+        start:"top 600vh",
+end:"bottom 20vh",
+    scrub:0.5,
+    }
+})
 
-
-// tl.from(".page2-text img",{
-//  scrollTrigger:{
-//     trigger:"#page2-text img",
-//     scroller:"body",
-//     opacity:1,
-//     delay:0.2,
-//     scale:1,
-//     scrub:1,
-//     start:"bottom 60vh",
+tl.from(".card1 p,.card2 p,.card3 p",{
+    opacity:0,
     
-    
-//     markers:true,
-//  }
+    y:-60,
+    scrollTrigger:{
+        trigger:".card p,.card2 p,.card3 p",
+        scroller:"body",
+       start:"top 190vh",
+    }
+})
 
-// })
+// ++-++
+
+tl.from(".image-section1 img:nth-child(1)",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".image-section1 img:nth-child(1)",
+        scroller:"body",
+        start:"top 600vh",
+        end:"bottom 30vh",
+    scrub:0.3,
+    }
+
+})
+// +++
+
+
+tl.from(".image-section1 img:nth-child(2)",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".image-section1 img:nth-child(2)",
+        scroller:"body",
+        start:"top 650vh",
+        end:"bottom 40vh",
+    scrub:0.3,
+    }
+})
+
+
+tl.from(".image-section1 img:nth-child(3)",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".image-section1 img:nth-child(3)",
+        scroller:"body",
+        start:"top 600vh",
+        end:"bottom 30vh",
+    scrub:0.3,
+    }
+
+})
+
+
+
+tl.from(".image-section1 img:nth-child(4)",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".image-section1 img:nth-child(4)",
+        scroller:"body",
+        start:"top 600vh",
+        end:"bottom 30vh",
+    scrub:0.3,
+    }
+
+})
+
+// ++++
+
+
+tl.from(".image-section2 img",{
+    opacity:1,
+    scale:0,
+    scrollTrigger:{
+        trigger:".image-section2 img",
+        scroller:"body",
+        start:"top 580vh",
+        end:"bottom 60vh",
+    scrub:0.3,
+    }
+
+})
+
+
+
